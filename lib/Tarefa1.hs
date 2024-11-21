139c3d7fda132730d72d35376f80f300802dfaaf
a107323 <a107323@alunos.uminho.pt>
2024-11-21 01:57:33 +0000

    mapa e validaPortal definidos

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -11,5 +11,38 @@ module Tarefa1 where
 
 import LI12425
 
+mapaJogo  :: Mapa
+mapaJogo = [[t, t, r, a, a, a],
+            [r, t, r, a, r, r],
+            [r, t, r, a, r, t],
+            [r, t, r, a, r, t],
+            [r, t, t, t, t, t],
+            [a, a, a, a, r, r]
+            ]
+      where
+            t = Terra
+            r = Relva
+            a = Agua
+
 validaJogo :: Jogo -> Bool
 validaJogo = undefined
+
+--  Verifica se todos os portais do jogo têm ondas sem inimigos no início.
+validaPortal :: [Portal] -> Bool
+validaPortal portais = all validaOndaPortal portais 
+  where
+            validaOndaPortal :: Portal -> Bool 
+            validaOndaPortal portal = all ondaSemInimigos (ondasPortal portal) 
+            
+            ondaSemInimigos :: Onda -> Bool 
+            ondaSemInimigos onda = null (inimigosOnda onda)
+
+validaInimigo :: Inimigo -> Bool
+validaInimigo = undefined
+
+validaTorre :: Torre -> Bool
+validaTorre = undefined
+
+validaBase :: Base -> Bool
+validaBase = undefined 
+
