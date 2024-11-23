f3715ec820ce20ff2342d2a1763057619abd2e90
a107323 <a107323@alunos.uminho.pt>
2024-11-23 12:01:09 +0000

    funções para ,Nao podem estar sobrepostos a torres ou  base, feita

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -28,7 +28,7 @@ mapaJogo = [[t, t, r, a, a, a],
 validaJogo :: Jogo -> Bool
 validaJogo = undefined
 
---  Verifica se todos os portais do jogo têm ondas sem inimigos no início
+--Verifica se todos os portais do jogo têm ondas sem inimigos no início
 validaPortal :: [Portal] -> Bool
 validaPortal portais = all validaOndaPortal portais 
       where
@@ -41,17 +41,31 @@ validaPortal portais = all validaOndaPortal portais
             minimoPortal :: [Portal] -> Bool
             minimoPortal portais = not (null portais)
 
-       --   Verifica se todos os portais estão posicionados sobre terra.
+            --Verifica se todos os portais estão posicionados sobre terra.
             posicionadoEmTerra :: Mapa -> [Portal] -> Bool
             posicionadoEmTerra mapa portais = all (\portal -> terrenoPorPosicao (posicaoPortal portal) mapa == Just Terra) portais
 
-            -- Obtém o terreno numa posição específica do mapa.
+            --Recebe  o terreno numa posição específica do mapa.
             terrenoPorPosicao :: Posicao -> Mapa -> Maybe Terreno
             terrenoPorPosicao (x, y) mapa =
                                           let linha = mapa !! (floor y) -- Converte Float para índice de linha
                                               terreno = linha !! (floor x) -- Converte Float para índice de coluna
                                            in Just terreno
                                                                  
+            naoSobrepostosTorreBase :: [Posicao] -> Base -> Torre -> Terreno -> Bool
+            naoSobrepostosTorreBase posicoes base torre terreno = all ( )
+                                                                  where 
+                                                                        posicionadoEmRelvaTorre :: Mapa -> [Torre] -> Bool
+                                                                        posicionadoEmRelvaTorre mapa torres = all (\torre -> terrenoPorPosicao (posicaoTorre torre) mapa == Just Relva) torres
+
+                                                                        posicionadoEmTerraBase :: Mapa -> Base -> Bool
+                                                                        posicionadoEmTerraBase mapa base = all (\base -> terrenoPorPosicao (posicaoBase base) mapa == Just Terra) base
+
+                                                                        verificaPosicaoTorreEmPortal :: Mapa -> [Torre] -> [Portal] -> Bool 
+                                                                        verificaPosicaoTorreEmPortal mapa torres portais = all (\portais -> posicionadoEmRelvaTorre (posicaoPortal portal) mapa /= posicaoTorre) portais
+
+                                                                        verificaPosicaoBaseEmPortal :: Mapa -> Base -> Bool 
+                                                                        verificaPosicaoBaseEmPortal mapa base = all (\base -> posicionadoEmTerraBase (posicaoBase base)  mapa /= posicaoBase) base
 
 validaInimigo :: Inimigo -> Bool
 validaInimigo = undefined
