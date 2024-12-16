c11f760dbe2b50bdf4a252628db85f5e880d5342
a107323 <a107323@alunos.uminho.pt>
2024-12-16 17:21:36 +0000

    EstadoJogo, EstadoMenu, funçoes para estados e desenhaApp e desenhaMenu

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,41 @@
 module Main where
 
-import Desenhar
-import Eventos
+import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss
-import ImmutableTowers
-import Tempo
-import LI2425
+import LI12425
 import Tarefa1
 import Tarefa2
 import Tarefa3
+import Eventos
+import Desenhar
+import Tempo
+-- Representa o estado do jogo.
+data EstadoJogo = EstadoJogo
+  { mapa :: Mapa
+  , moedas :: Int
+  , torres :: [Torre]
+  , inimigos :: [Inimigo]
+  , base :: Base
+  }
 
-data MenuInicialOpcoes = Jogar | Sair  deriving (Show, Eq)
+
+data EstadoMenu = MenuPrincipal | Jogando EstadoJogo | Sair 
+
+data EstadoApp = EstadoApp
+  { estadoAtual :: EstadoMenu
+  }
+
+estadoInicialApp :: EstadoApp
+estadoInicialApp = EstadoApp { estadoAtual = MenuPrincipal }
+
+estadoInicialJogo :: EstadoJogo
+estadoInicialJogo = EstadoJogo
+  { mapa = mapa1
+  , moedas = creditosBase base1
+  , torres = []
+  , inimigos = [inimigo1]
+  , base = base1
+  }
 
 janela :: Display
 janela = InWindow "Immutable Towers" (1920, 1080) (0, 0)
@@ -22,9 +47,18 @@ fr :: Int
 fr = 60
 
 main :: IO ()
-main = do
-  putStrLn "Hello from Immutable Towers!"
+main = play janela fundo fr estadoInicialApp desenhaApp reageEventosApp reageTempoApp
 
-  play janela fundo fr it desenha reageEventos reageTempo
-  where
-    it = ImmutableTowers {}
+desenhaApp :: EstadoApp -> Picture
+desenhaApp app = case estadoAtual app of
+  MenuPrincipal -> desenhaMenu
+  Jogando jogo -> desenhaJogo jogo
+  Sair -> Blank
+
+
+desenhaMenu :: Picture
+desenhaMenu = Pictures
+  [ Translate (-300) 100 . Scale 0.5 0.5 . Color black $ Text "Menu Principal"
+  , Translate (-200) 0 . Scale 0.3 0.3 . Color blue $ Text "1. Jogar (Enter ou Clique)"
+  , Translate (-200) (-50) . Scale 0.3 0.3 . Color red $ Text "2. Sair (Esc ou Clique)"
+  ]
