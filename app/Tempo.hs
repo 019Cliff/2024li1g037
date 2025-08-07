499544af2ed8de935bd806f3cedb558484c6358b
cliff <a107323@alunos.uminho.pt>
2025-08-07 13:59:58 +0100

    reageTempo

--- app/Tempo.hs
+++ app/Tempo.hs
@@ -2,8 +2,17 @@ module Tempo where
 
 import ImmutableTowers
 import LI12425
-import Tarefa3
+import Tarefa3 (atualizaJogo)
 
-reageTempo :: Float -> ImmutableTowers -> IO ImmutableTowers
+reageTempo :: Tempo -> ImmutableTowers -> IO ImmutableTowers
+reageTempo segundos estado@(ImmutableTowers jogo imgs EmJogo tempoAtual torreSel) =
+  let segundosFloat = realToFrac segundos
+
+      -- Atualiza o jogo inteiro com o tempo decorrido
+      jogoAtualizado = atualizaJogo segundosFloat jogo
+
+      -- Tempo acumulado
+      tempoNovo = tempoAtual + segundos
+   in return $ ImmutableTowers jogoAtualizado imgs EmJogo tempoNovo torreSel
+-- Outros modos não alteram o estado
 reageTempo _ estado = return estado
-
